618462b6ba312226da3c334722f71c5ed451dc9f
bbi <dmsql3335@gmail.com>
2024-09-25 01:45:22 +0900

    폴더, 페이지, 경로

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Tomato() {
+  return <h1>Hello</h1>;
+}
+
+// 작동방식
+// react-router ---> <Home />이라는 컴포넌트 render를 요청하는거
+// about-us ---> <AboutUs />
+// movies/:id ---> <Movie />
+// 수동적인 방식들
